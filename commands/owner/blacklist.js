27807c785eb9b61fa6e93a18327a59bf83decb87
Team Eclipse <40067192-teameclipsebest@users.noreply.replit.com>
2025-02-27 17:35:29 +0000

    Assistant checkpoint: Fixed syntax error in blacklist.js
    Assistant generated file changes:
    - commands/owner/blacklist.js: Fix syntax error by merging duplicate constructor methods
    
    ---
    
    User prompt:
    
    Running the code in my project results in the following:
    
    ```
    /home/runner/workspace/commands/owner/blacklist.js:164
        constructor(client) {
                            ^
    
    SyntaxError: Unexpected token '{'
        at wrapSafe (node:internal/modules/cjs/loader:1378:20)
        at Module._compile (node:internal/modules/cjs/loader:1428:41)
        at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
        at Module.load (node:internal/modules/cjs/loader:1288:32)
        at Module._load (node:internal/modules/cjs/loader:1104:12)
        at Module.require (node:internal/modules/cjs/loader:1311:19)
        at require (node:internal/modules/helpers:179:18)
        at /home/runner/workspace/core/handler.js:40:36
        at Array.forEach (<anonymous>)
        at /home/runner/workspace/core/handler.js:38:47
    
    Node.js v20.18.1
    
    ```
    
    If applicable, propose a fix immediately.

--- commands/owner/blacklist.js
+++ commands/owner/blacklist.js
@@ -1,5 +1,5 @@
-const { EmbedBuilder } = require("discord.js");
 const BaseCommand = require("../../assets/baseCmd");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = class Blacklist extends BaseCommand {
     constructor(client) {
@@ -24,6 +24,7 @@ module.exports = class Blacklist extends BaseCommand {
             usage: "blacklist user 123456789"
         });
     }
+
     async run(message, args, prefix) {
         let opt = args[0];
 
@@ -59,10 +60,10 @@ module.exports = class Blacklist extends BaseCommand {
                 ]
             });
         }
-        
+
         if(opt.toLowerCase() === "user") {
             let user = message.mentions.users.first() || this.client.users.cache.get(args[1]) || await this.client.users.fetch(args[1]).catch(() => {});
-            
+
             if(!user) {
                 return await message.channel.send({
                     embeds: [
@@ -117,13 +118,13 @@ module.exports = class Blacklist extends BaseCommand {
                     ]
                 });
             }
-            
+
             const id = args[1];
-            
+
             if(blacklistedUsers.includes(id)) {
                 blacklistedUsers = blacklistedUsers.filter(userId => userId !== id);
                 await this.client.db.updateBlacklist({ blacklistedUsers });
-                
+
                 return message.channel.send({
                     embeds: [
                         new EmbedBuilder()
@@ -134,7 +135,7 @@ module.exports = class Blacklist extends BaseCommand {
             } else if(blacklistedServers.includes(id)) {
                 blacklistedServers = blacklistedServers.filter(serverId => serverId !== id);
                 await this.client.db.updateBlacklist({ blacklistedServers });
-                
+
                 return message.channel.send({
                     embeds: [
                         new EmbedBuilder()
@@ -153,150 +154,6 @@ module.exports = class Blacklist extends BaseCommand {
             }
         } else {
             return await message.channel.send({
-                embeds: [
-                    new EmbedBuilder().setColor(this.client.config.color)
-                    .setDescription(`${this.client.emoji.cross} Invalid option! Use user/server/remove/list.`)
-                ]
-            });
-        }
-    }
-}
-    constructor(client) {
-        super(client, {
-            name: "blacklist",
-            aliases: ["bl"],
-            cat: "owner",
-            args: true,
-            desc: "Blacklist a user or server from using the bot",
-            options: ["user/server <ID>", "remove <ID>", "list"],
-            ownerOnly: true,
-            usage: "blacklist user 123456789"
-        });
-    }
-    async run(message, args, prefix) {
-        if (!args[0]) {
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(this.client.config.color)
-                        .setDescription(`${this.client.emoji.cross} Please provide an option! (user/server/remove/list)`)
-                ]
-            });
-        }
-
-        const option = args[0].toLowerCase();
-        const npData = await this.client.db.getNoPrefixList();
-        let blacklistedUsers = npData?.blacklistedUsers || [];
-        let blacklistedServers = npData?.blacklistedServers || [];
-
-        if (option === "list") {
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(this.client.config.color)
-                        .setTitle("Blacklisted Entities")
-                        .addFields([
-                            {
-                                name: "Users",
-                                value: blacklistedUsers.length ? blacklistedUsers.join("\n") : "No blacklisted users"
-                            },
-                            {
-                                name: "Servers",
-                                value: blacklistedServers.length ? blacklistedServers.join("\n") : "No blacklisted servers"
-                            }
-                        ])
-                ]
-            });
-        }
-
-        if (!args[1] && option !== "list") {
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(this.client.config.color)
-                        .setDescription(`${this.client.emoji.cross} Please provide an ID!`)
-                ]
-            });
-        }
-
-        const id = args[1];
-
-        if (option === "user") {
-            if (blacklistedUsers.includes(id)) {
-                return message.channel.send({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(this.client.config.color)
-                            .setDescription(`${this.client.emoji.cross} This user is already blacklisted!`)
-                    ]
-                });
-            }
-
-            blacklistedUsers.push(id);
-            await this.client.db.updateBlacklist({ blacklistedUsers });
-
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(this.client.config.color)
-                        .setDescription(`${this.client.emoji.tick} Successfully blacklisted user with ID: ${id}`)
-                ]
-            });
-        } else if (option === "server") {
-            if (blacklistedServers.includes(id)) {
-                return message.channel.send({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(this.client.config.color)
-                            .setDescription(`${this.client.emoji.cross} This server is already blacklisted!`)
-                    ]
-                });
-            }
-
-            blacklistedServers.push(id);
-            await this.client.db.updateBlacklist({ blacklistedServers });
-
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(this.client.config.color)
-                        .setDescription(`${this.client.emoji.tick} Successfully blacklisted server with ID: ${id}`)
-                ]
-            });
-        } else if (option === "remove") {
-            if (blacklistedUsers.includes(id)) {
-                blacklistedUsers = blacklistedUsers.filter(userId => userId !== id);
-                await this.client.db.updateBlacklist({ blacklistedUsers });
-
-                return message.channel.send({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(this.client.config.color)
-                            .setDescription(`${this.client.emoji.tick} Successfully removed user ${id} from blacklist!`)
-                    ]
-                });
-            } else if (blacklistedServers.includes(id)) {
-                blacklistedServers = blacklistedServers.filter(serverId => serverId !== id);
-                await this.client.db.updateBlacklist({ blacklistedServers });
-
-                return message.channel.send({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(this.client.config.color)
-                            .setDescription(`${this.client.emoji.tick} Successfully removed server ${id} from blacklist!`)
-                    ]
-                });
-            } else {
-                return message.channel.send({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(this.client.config.color)
-                            .setDescription(`${this.client.emoji.cross} This ID is not blacklisted!`)
-                    ]
-                });
-            }
-        } else {
-            return message.channel.send({
                 embeds: [
                     new EmbedBuilder()
                         .setColor(this.client.config.color)
